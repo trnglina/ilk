c82b697afdaf872dd41372a2b45e32704d5b1d98
Teng Liu <teng@trnglina.org>
2026-09-22 19:50:14 +0900

    Scaffold compiler CLI

--- crates/ilkc/src/main.rs
+++ crates/ilkc/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs, path::PathBuf};
+
+use anyhow::Result;
+use clap::{Parser, ValueEnum};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    out_dir: PathBuf,
+    #[arg(short, long, value_enum, default_value_t = Format::Souffle)]
+    format: Format,
+
+    file: PathBuf,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum Format {
+    Souffle,
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+    let source = fs::read_to_string(&cli.file)?;
+
+    Ok(())
 }
